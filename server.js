0e541490fb39db8fafc2774c700655e84a672b3e
Matt Disalov <61394693+mattdisalov@users.noreply.github.com>
2020-11-30 19:24:21 -0500

    updated server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-
+const path = require('path');
 const app = express();
 
 app.use(fileUpload());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Upload Endpoint
 app.post('/upload', (req, res) => {
